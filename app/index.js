48f284a6896789f005c70c64b70adca37f6b639b
Daniel Moody <dmoody89@tutamail.com>
2020-04-23 16:09:57 +0100

    Working on search function

--- app/index.js
+++ app/index.js
@@ -11,6 +11,8 @@ app.get('about', (req, res) => res.sendFile('about.html'));
 app.get('contact', (req, res) => res.sendFile('contact.html'));
 
 app.get('/recipes/all', (req, res) => res.json(recipes));
-app.get('/recipes', (req, res) => res.json(recipes));
+app.get('/recipes', (req, res) => {
+   console.log(req.query.tag);
+});
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
